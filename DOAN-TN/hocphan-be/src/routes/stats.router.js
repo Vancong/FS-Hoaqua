32813d97245a57075ff11b4c6a632d47dea177cf
Tran Van Cong <congtv.bhsoft@gmail.com>
2026-05-28 19:01:23 +0700

    done

--- DOAN-TN/hocphan-be/src/routes/stats.router.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const statsControllers=require('../controllers/stats.controllers');
-const {authMiddleware}=require("../middleware/auth.middleware");
-
-
-router.get("/revenue",authMiddleware, statsControllers.revenue);  
-
-module.exports=router;
